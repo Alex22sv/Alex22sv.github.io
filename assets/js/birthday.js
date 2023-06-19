ada38ba5abb143786b547bec033657b124fcfa4c
Alex22sv <ae22mp@gmail.com>
2023-06-19 15:44:16 -0600

    Actualizar mensaje de cumpleaños

--- assets/js/birthday.js
+++ assets/js/birthday.js
@@ -11,7 +11,7 @@
 	  mm = String(today.getMonth() + 1).padStart(2, "0"),  
 	  yyyy = today.getFullYear(),  
 	  nextYear = yyyy + 1,  
-	  dayMonth = "06/22/",  
+	  dayMonth = "06/19/",  
 	  birthday = dayMonth + yyyy;  
 	today = mm + "/" + dd + "/" + yyyy;  
 	if (today > birthday) {  
